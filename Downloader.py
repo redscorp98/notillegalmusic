33ab2db6ce5c0a8ac79fd07e2f295bda5a88409d
redscorp98 <37073183+redscorp98@users.noreply.github.com>
2018-03-11 23:31:32 +0000

    Started converting to object layout, comments left in code, confused about whether to have a couple separate objects for youtube_dl and meta data editing

--- Downloader.py
+++ Downloader.py
@@ -2,22 +2,18 @@ from __future__ import unicode_literals
 import youtube_dl as yt_dl, urllib, re, eyed3, os, subprocess 
 from subprocess import call
 
-def download(artist, title, track_num, album_artist, album, playlistname, number_items):
-    filename = createfilename(title,artist)
+def download(artist, title, track_num, album_artist, album, playlistname):
+    song = Downloader() # not sure how best to call the object???
+    filename = song.combine(title,artist)
     output_folder = os.getcwd() + "\\" + playlistname + "\\"  #should i make this global rather than have it done every time the function is called?
-    if not(os.path.isfile(os.getcwd()+ "\\"+playlistname+"\\"+filename+".mp3")):       
-        ydl_opts = {'quiet':True, 'outtmpl' : '/' + playlistname + '/'+filename+'.%(ext)s', 'format': 'bestaudio/best',
-        'postprocessors': [{
-        'key': 'FFmpegExtractAudio',
-        'preferredcodec': 'mp3',
-        'preferredquality': '192',}], 'output':'video'}
+    if not(os.path.isfile(os.getcwd()+ "\\"+playlistname+"\\"+filename+".mp3")):           
         print("Download Begin")
         query_string = urllib.parse.urlencode({"search_query" : artist + " " + title})  #where the user input
         html_content = urllib.request.urlopen("http://www.youtube.com/results?" + query_string)
         search_results = re.findall(r'href=\"\/watch\?v=(.{11})', html_content.read().decode())
-        with yt_dl.YoutubeDL(ydl_opts) as ydl:
-            ydl.download(['http://www.youtube.com/watch?v=' + search_results[0]])
-        audio = eyed3.load(output_folder+filename+".mp3")
+        ydl = yt_dl.YoutubeDL(song.ydl_config(playlistname,filename))
+        ydl.download(['http://www.youtube.com/watch?v=' + search_results[0]])
+        audio = eyed3.load(output_folder+filename+".mp3")#should i make tagging a object?
         audio.tag.artist = artist
         audio.tag.album = album
         audio.tag.album_artist = album_artist
@@ -28,6 +24,16 @@ def download(artist, title, track_num, album_artist, album, playlistname, number
     else:
         print("File found in directory...")
 
-def createfilename(back, lead):
-    collect = lead + " - " + back
-    return collect
+
+class Downloader(object):
+    def ydl_config(self,playlistname, filename):
+        ydl_opts = {'quiet':True, 'outtmpl' : '/' + playlistname + '/'+filename+'.%(ext)s', 'format': 'bestaudio/best',
+                'postprocessors': [{
+                'key': 'FFmpegExtractAudio',
+                'preferredcodec': 'mp3',
+                'preferredquality': '192',}], 'output':'video'}
+        return ydl_opts 
+
+    def combine(self,back, lead):
+        collect = lead + " - " + back
+        return collect
